eeb673a5e4110f2d4ff3e14b2cc2b36192f971fc
Ilya Lokalin <ilyalokalin@gmail.com>
2024-04-03 02:12:36 +0300

    feat: add Authorization

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function enableCORS(req, res, next) {
   res.header('Access-Control-Allow-Credentials', true)
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Content-Encoding, Accept'
+    'Origin, X-Requested-With, Content-Type, Content-Encoding, Accept, Authorization' // Add Authorization header
   )
   res.header(
     'Access-Control-Allow-Methods',
